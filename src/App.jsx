5665ecb807cc9cfa53e4df27c09a276adc93a413
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 16:11:24 +0300

    Reorganized the imports for the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from './components/Navbar'
-import CartContainer from './components/CartContainer'
-import { useDispatch, useSelector } from 'react-redux'
-import { calculateTotals } from './features/cart/cartSlice'
 import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CartContainer from './components/CartContainer'
 import Modal from './components/Modal'
+import Navbar from './components/Navbar'
+import { calculateTotals } from './features/cart/cartSlice'
 
 function App() {
 	const dispatch = useDispatch()
