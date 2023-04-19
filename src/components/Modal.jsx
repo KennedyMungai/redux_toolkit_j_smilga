9e6192ec97c3c29532042632ece0c0e8ac06af3c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 16:18:06 +0300

    Added onClick event handlers to both the confirm and the cancel buttons

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,8 +6,12 @@ const Modal = () => {
 			<div className='modal'>
 				<h4>Remove all items from your shopping cart?</h4>
 				<div className='btn-container'>
-					<button className='btn confirm-btn'>Confirm</button>
-					<button className='btn clear-btn'>Cancel</button>
+					<button className='btn confirm-btn' onClick={() => {}}>
+						Confirm
+					</button>
+					<button className='btn clear-btn' onClick={() => {}}>
+						Cancel
+					</button>
 				</div>
 			</div>
 		</aside>
