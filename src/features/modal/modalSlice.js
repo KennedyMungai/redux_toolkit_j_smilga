8f7a1d25796dbe1d6d7aec7f17ceafcc27bf5eec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 16:06:25 +0300

    Created the initial state for the slice

--- src/features/modal/modalSlice.js
+++ src/features/modal/modalSlice.js
@@ -1,7 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+	isOpen: false
+}
+
 export const modalSlice = createSlice({
 	name: 'modal',
-	initialState: {},
+	initialState,
 	reducers: {}
 })
