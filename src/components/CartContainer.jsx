761146a80bb68abcac70143fe71fe90c757935e2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 16:17:21 +0300

    To clear the cart, dispatched the open modal function instead of the clear cart funnction

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -3,6 +3,7 @@ import CartItem from './CartItem'
 import { useDispatch, useSelector } from 'react-redux'
 import cartItems from '../cartItems'
 import { clearCart } from '../features/cart/cartSlice'
+import { openModal } from '../features/modal/modalSlice'
 
 const CartContainer = () => {
 	const dispatch = useDispatch()
@@ -39,7 +40,7 @@ const CartContainer = () => {
 				</div>
 				<button
 					className='btn clear-btn'
-					onClick={() => dispatch(clearCart())}
+					onClick={() => dispatch(openModal())}
 				>
 					Clear Cart
 				</button>
