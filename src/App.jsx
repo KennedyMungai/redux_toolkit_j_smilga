ebd5df6a70f1d2626d7aba9ca139cd415a56279f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 15:43:57 +0300

    Created an instance of useDispatch called dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
+import { useDispatch } from 'react-redux'
 
 function App() {
+	const dispatch = useDispatch()
+
 	return (
 		<main>
 			<Navbar />
