592202e5a919d06654cfe4abe621923160c3eed2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 15:42:07 +0300

    Create dthe calculateTotals action

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -35,7 +35,19 @@ const cartSlice = createSlice({
 
 			state.amount -= 1
 		},
-		calculateTotals: (state) => {}
+		calculateTotals: (state) => {
+			let amount = 0
+			let total = 0
+
+			state.cartItems.forEach(
+				(item) => (
+					(amount += item.amount), (total += item.amount * item.price)
+				)
+			)
+
+			state.amount = amount
+			state.total = total
+		}
 	}
 })
 
