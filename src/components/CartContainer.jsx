3e297225b89f0b4babeecd29e37e233af9220389
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 14:32:39 +0300

    Destructured cartItem , total and amount form the cart state

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -3,6 +3,8 @@ import CartItem from './CartItem'
 import { useSelector } from 'react-redux'
 
 const CartContainer = () => {
+	const { cartItem, total, amount } = useSelector((state) => state.cart)
+
 	return <div>cartContainer</div>
 }
 
