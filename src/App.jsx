0df7f4a4d5412ecd450419b2b735ea65929e6a1d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 15:45:25 +0300

    Exposed the cartItems and made the useEffect function block run everyTime cartItems changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { calculateTotals } from './features/cart/cartSlice'
 
 function App() {
 	const dispatch = useDispatch()
+	const { cartItems } = useSelector((state) => state.cart)
+
+	useEffect(() => {
+		dispatch(calculateTotals())
+	}, [cartItems])
 
 	return (
 		<main>
