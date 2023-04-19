fc88cd388f8198e5ca35812162157bce2ea2c8c4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 15:45:57 +0300

    Added an import for the useEffect component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 import { useDispatch, useSelector } from 'react-redux'
 import { calculateTotals } from './features/cart/cartSlice'
+import { useEffect } from 'react'
 
 function App() {
 	const dispatch = useDispatch()
