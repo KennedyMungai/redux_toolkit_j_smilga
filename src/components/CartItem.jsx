e04b8512117670a57d744e2a4c2711198a7132e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 14:51:24 +0300

    Added the name and price details for the CartItem component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -5,6 +5,10 @@ const CartItem = ({ id, img, title, price, amount }) => {
 	return (
 		<article className='cart-item'>
 			<img src={img} alt={title} />
+			<div>
+				<h4>{title}</h4>
+				<h4 className='item-price'>${price}</h4>
+			</div>
 		</article>
 	)
 }
