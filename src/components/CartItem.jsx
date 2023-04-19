0acfe7228011129c16a514d0e764080ae6971fba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 14:50:14 +0300

    Set it up so that the CartItem can be peoperly and clearly rendered

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { BsChevronUp, BsChevronDown } from 'react-icons/bs'
 
 const CartItem = ({ id, img, title, price, amount }) => {
-	return <div>cartItem</div>
+	return (
+		<article className='cart-item'>
+			<img src={img} alt={title} />
+		</article>
+	)
 }
 
 export default CartItem
