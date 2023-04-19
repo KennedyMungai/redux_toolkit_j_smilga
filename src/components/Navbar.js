dedcbb4a34926b79b0b9080443fa65d1ca4b8f23
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-19 14:08:52 +0300

    Wrote the markup for the Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,20 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const Navbar = () => {
-	const { ampunt } = useSelector((state) => state.cart)
+	const { amount } = useSelector((state) => state.cart)
 	const dispatch = useDispatch()
 
-	return <div>Navbar</div>
+	return (
+		<nav>
+			<h3>redux toolkit</h3>
+			<div className='nav-container'>
+				{/* <CartIcon /> */}
+				<div className='amount-container'>
+					<p className='total-amount'>{amount}</p>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
