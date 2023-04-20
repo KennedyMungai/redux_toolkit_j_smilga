c081efd5a153e3242f5034801f97b2f99cbbea01
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-04-20 10:26:15 +0300

    Returned data from teh response

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -13,7 +13,7 @@ const initialState = {
 export const getCartItems = createAsyncThunk('cart/getCartItems', async () => {
 	try {
 		const resp = await axios(url)
-		return resp
+		return { data }
 	} catch (error) {}
 })
 
